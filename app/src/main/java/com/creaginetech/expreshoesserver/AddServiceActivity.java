abd852e3e3a0f73499bc24e5c828eeb0705376cc
firlanaryu <firlanasanur@gmail.com>
2019-04-05 15:43:26 +0700

    edit-update-service-part-2

---
+++ app/src/main/java/com/creaginetech/expreshoesserver/AddServiceActivity.java
@@ -0,0 +1,210 @@
+package com.creaginetech.expreshoesserver;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.creaginetech.expreshoesserver.Common.Common;
+import com.creaginetech.expreshoesserver.Model.Service;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import java.util.UUID;
+
+public class AddServiceActivity extends AppCompatActivity {
+
+    //Firebase database
+    FirebaseDatabase database;
+    DatabaseReference serviceRef;
+
+    //firebase storage
+    FirebaseStorage storage;
+    StorageReference storageRef;
+
+    //Add new Menu Layout
+    EditText edtName,edtPrice,edtDesc;
+    Button btnSelect,btnSave,btnCancel;
+    ImageView imgService;
+
+    //select image
+    Uri saveUri;
+
+    String imgUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_service);
+        setTitle("Setup New Service");
+
+        //Init Firebase
+        database = FirebaseDatabase.getInstance();
+        serviceRef = database.getReference("Service").child(Common.currentUser);
+
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
+
+        edtName = findViewById(R.id.edtName);
+        edtPrice = findViewById(R.id.edtPrice);
+        edtDesc = findViewById(R.id.edtDesc);
+        btnSelect = findViewById(R.id.btnSelect);
+        btnSave = findViewById(R.id.buttonSave);
+        btnCancel = findViewById(R.id.buttonCancel);
+        imgService = findViewById(R.id.imageViewService);
+
+        //Event for button
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectImage();
+            }
+        });
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (saveUri == null){
+                    Toast.makeText(getApplicationContext(), "Choose image", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (edtName.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(), "Enter Service name", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (edtPrice.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(), "Enter Service price", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (edtDesc.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(), "Enter Service description", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                saveService();
+
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(AddServiceActivity.this, HomeNewActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+
+    private void selectImage() {
+
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"Select picture"),Common.PICK_IMAGE_REQUEST);
+
+    }
+
+    //SELECT IMAGE
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == Common.PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null)
+        {
+            saveUri = data.getData();
+            btnSelect.setText("Image Selected !");
+            imgService.setImageURI(saveUri);
+
+        }
+    }
+
+    public void saveService(){
+
+        if (saveUri != null)
+        {
+
+            final ProgressDialog mDialog = new ProgressDialog(AddServiceActivity.this);
+            mDialog.show();
+
+            String imageName = UUID.randomUUID().toString();
+            final StorageReference imageFolder = storageRef.child("expreshoes/serviceImages/"+imageName);
+            imageFolder.putFile(saveUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                            Toast.makeText(AddServiceActivity.this, "Uploaded !!!", Toast.LENGTH_SHORT).show();
+                            imageFolder.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+
+                                    mDialog.dismiss();
+
+                                    imgUri = uri.toString();
+
+                                    pushToDatabase();
+
+                                }
+                            });
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            mDialog.dismiss();
+                            Toast.makeText(AddServiceActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                            mDialog.setMessage("Uploaded "+progress+" %");
+
+                        }
+                    });
+        }
+
+    }
+
+    private void pushToDatabase(){
+
+        String namaService = edtName.getText().toString().trim();
+        String priceService = edtPrice.getText().toString().trim();
+        String descService = edtDesc.getText().toString().trim();
+        String idService = serviceRef.push().getKey();
+
+        Service newService = new Service(namaService, imgUri, descService, priceService);
+        serviceRef.child(idService).setValue(newService);
+
+        Intent intent = new Intent(AddServiceActivity.this, HomeNewActivity.class);
+        Toast.makeText(AddServiceActivity.this,
+                "New Service was added", Toast.LENGTH_LONG).show();
+        startActivity(intent);
+
+    }
+
+}
---
+++ app/src/main/java/com/creaginetech/expreshoesserver/ServiceDetailActivity.java
@@ -0,0 +1,262 @@
+package com.creaginetech.expreshoesserver;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.creaginetech.expreshoesserver.Common.Common;
+import com.creaginetech.expreshoesserver.Model.Service;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class ServiceDetailActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText service_name,service_price,service_description;
+    ImageView service_image;
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    Button btnUpdateItem,btnDeleteItem;
+
+    String serviceId="";
+
+    String imgUri;
+
+    //select image
+    Uri saveUri;
+
+    FirebaseAuth mAuth;
+    FirebaseUser mFirebaseUser;
+    FirebaseDatabase database;
+    DatabaseReference service;
+
+    //firebase storage
+    FirebaseStorage storage;
+    StorageReference storageRef;
+
+    Service currentService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_detail);
+
+        //Firebase
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mAuth.getCurrentUser();
+        database = FirebaseDatabase.getInstance();
+        service = database.getReference("Service").child(mFirebaseUser.getUid()); //tambahi child shopId
+
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
+
+        service_description = findViewById(R.id.service_description);
+        service_name = findViewById(R.id.service_name);
+        service_price = findViewById(R.id.service_price);
+        service_image = findViewById(R.id.img_service);
+        btnUpdateItem = findViewById(R.id.btnUpdateItem);
+        btnDeleteItem = findViewById(R.id.btnDeleteItem);
+
+        btnUpdateItem.setOnClickListener(this);
+        btnDeleteItem.setOnClickListener(this);
+        service_image.setOnClickListener(this);
+
+        //Collapsing Tool bar
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing);
+        collapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.ExpandedAppbar);
+        collapsingToolbarLayout.setCollapsedTitleTextAppearance(R.style.CollapsedAppbar);
+
+        serviceId = Common.serviceSelected;
+        getDetailService(serviceId);
+    }
+
+    private void getDetailService(String serviceId) {
+
+        service.child(serviceId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                currentService = dataSnapshot.getValue(Service.class);
+
+                //Set Image
+                Picasso.with(getBaseContext()).load(currentService.getServiceImage())
+                        .into(service_image);
+//                collapsingToolbarLayout.setTitle(currentService.getServiceName());
+                service_price.setText(currentService.getPrice());
+                service_name.setText(currentService.getServiceName());
+                service_description.setText(currentService.getDescription());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        int i = view.getId();
+        if (i == R.id.btnUpdateItem){
+            updateItem();
+        } else if (i == R.id.btnDeleteItem){
+            deleteItem();
+        }else if (i == R.id.img_service){
+            selectImage();
+        }
+    }
+
+    //[START TO OPEN STORAGE TO SELECT IMAGE] - 1
+    private void selectImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"Select picture"),Common.PICK_IMAGE_REQUEST);
+    }
+    //[END TO OPEN STORAGE TO SELECT IMAGE] - 1
+
+    //[START DELETE ITEM]
+    private void deleteItem() {
+//        final StorageReference imageFolder = storageRef.child("expreshoes/serviceImages/"+currentService.getServiceImage());
+        final StorageReference imageFolder = storage.getReferenceFromUrl(currentService.getServiceImage());
+        imageFolder.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Intent homeIntent = new Intent(ServiceDetailActivity.this,HomeNewActivity.class);
+                startActivity(homeIntent);
+                service.child(serviceId).removeValue();
+                Toast.makeText(ServiceDetailActivity.this, "Item Deleted !", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(ServiceDetailActivity.this, "Failed to delete item", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    //[END DELETE ITEM]
+
+    //[START UPDATE ITEM]
+    private void updateItem() {
+        //jika user mengganti semua item termasuk gambar
+        if (saveUri != null)
+        {
+            final ProgressDialog mDialog = new ProgressDialog(ServiceDetailActivity.this);
+            mDialog.show();
+
+            String imageName = UUID.randomUUID().toString();
+            final StorageReference imageFolder = storageRef.child("expreshoes/serviceImages/"+imageName);
+//            final StorageReference imageFolder = storage.getReferenceFromUrl(currentService.getServiceImage());
+            imageFolder.putFile(saveUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(ServiceDetailActivity.this, "Uploaded !!!", Toast.LENGTH_SHORT).show();
+                            imageFolder.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    mDialog.dismiss();
+
+                                    //img to string
+                                    imgUri = uri.toString();
+                                    //push update to database
+                                    pushToDatabase();
+                                }
+                            });
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            mDialog.dismiss();
+                            Toast.makeText(ServiceDetailActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                            mDialog.setMessage("Uploaded "+progress+" %");
+                        }
+                    });
+        }
+        else {
+            //jika user hanya mengganti service name,price dan desc tanpa mengganti gambar
+            final StorageReference imageFolder = storage.getReferenceFromUrl(currentService.getServiceImage());
+            imageFolder.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    //img to string
+                    imgUri = uri.toString();
+                    //save to database
+                    pushToDatabase();
+                }
+            });
+        }
+    }
+
+    //[START push to update database]
+    private void pushToDatabase(){
+        Map<String,Object> update_item = new HashMap<>();
+        update_item.put("serviceName",service_name.getText().toString());
+        update_item.put("price",service_price.getText().toString());
+        update_item.put("description",service_description.getText().toString());
+        update_item.put("serviceImage",imgUri); //save image URL string
+
+        service.child(serviceId).updateChildren(update_item).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful())
+                    Toast.makeText(ServiceDetailActivity.this, "Item was updated !", Toast.LENGTH_SHORT).show();
+                Intent homeIntent = new Intent(ServiceDetailActivity.this,HomeNewActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        });
+    }
+    //[END push to update database]
+
+    //[START TO OPEN STORAGE TO SELECT IMAGE] - 2 (GET IMAGE FROM STORAGE)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == Common.PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null)
+        {
+            saveUri = data.getData();
+            service_image.setImageURI(saveUri);
+        }
+    }
+    //[END TO OPEN STORAGE TO SELECT IMAGE] - 2 (GET IMAGE FROM STORAGE)
+
+
+}
